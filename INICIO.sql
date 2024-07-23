dba9dc85bfc078b699562ff81386e635cfca9852
TonyGarcia5s <anthony.garcia2908@gmail.com>
2024-07-23 13:37:02 -0600

    Create INICIO.sql
    Prueba

---
+++ INICIO.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Sucursal (
+    SucursalID INT PRIMARY KEY,
+    Nombre VARCHAR(100) NOT NULL,
+    Dirección VARCHAR(200) NOT NULL,
+    Ciudad VARCHAR(100) NOT NULL,
+    Provincia VARCHAR(100) NOT NULL,
+    Teléfono VARCHAR(15)
+);
