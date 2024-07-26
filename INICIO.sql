a7a52271db476c6ff43b378d34fb1e62f52f4db7
TonyGarcia5s <anthony.garcia2908@gmail.com>
2024-07-25 18:13:59 -0600

    INTERFAZ

--- INICIO.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE Sucursal (
-    SucursalID INT PRIMARY KEY,
-    Nombre VARCHAR(100) NOT NULL,
-    Dirección VARCHAR(200) NOT NULL,
-    Ciudad VARCHAR(100) NOT NULL,
-    Provincia VARCHAR(100) NOT NULL,
-    Teléfono VARCHAR(15)
-);
